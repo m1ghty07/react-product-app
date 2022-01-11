61bdb0f759550bd751682180029ca74dbd9f39a4
m1ghty07 <dulchenko_s@ukr.net>
2022-01-11 18:05:39 +0200

    add filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -12,6 +12,7 @@ class App extends Component {
     super(props);
     this.state = {
       data: [],
+      term: "",
     };
     this.maxId = 4;
   }
@@ -69,13 +70,29 @@ class App extends Component {
     }));
   };
 
+  searchProducts = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+
+    return items.filter((item) => {
+      return item.name.indexOf(term) > -1;
+    });
+  };
+
+  onUpdateSearch = (term) => {
+    this.setState({ term });
+  };
+
   render() {
+    const { data, term } = this.state;
     const total = this.state.data.length;
     const withDiscount = this.state.data.filter((item) => item.discount).length;
     let totalPrice = 0;
     this.state.data.map((item) => {
       return (totalPrice += +item.price);
     });
+    const visibleData = this.searchProducts(data, term);
 
     return (
       <div className="app">
@@ -86,12 +103,12 @@ class App extends Component {
         />
 
         <div className="search-panel">
-          <SearchPanel />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
           <AppFilter />
         </div>
 
         <ShoppingList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           toggleDiscount={this.toggleDiscount}
           toggleBought={this.toggleBought}
